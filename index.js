0cd7a8ac3d0a662c525c0737d1f04b5c54b9877d
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-02-27 21:44:28 +1300

    Remove comments

--- index.js
+++ index.js
@@ -8,27 +8,22 @@ const token = process.env.FB_PAGE_ACCESS_TOKEN;
 
 app.set('port', (process.env.PORT || 5000));
 
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Process application/json
 app.use(bodyParser.json());
 
-// Index route
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 });
 
-// for Facebook verification
 app.get('/webhook/', function (req, res) {
     console.log(req);
     console.log(req.query);
-    if (req.query['hub.verify_token'] === 'asdfqwervawe1235asAASDQ') {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 });
-
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text };
@@ -62,9 +57,6 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 });
 
-
-
-// Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 });

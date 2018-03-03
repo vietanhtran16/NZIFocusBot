83d3acd5c5fa33f17c1c3e78e94f788485df65bb
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-03-03 21:43:52 +1300

    Add block to check if intent exist or not

--- src/responses/createResponse.js
+++ src/responses/createResponse.js
@@ -3,11 +3,16 @@ import WeatherApi from "./WeatherApi";
 const weatherApi = new WeatherApi(process.env.OPEN_WEATHER_TOKEN);
 
 export async function createResponse(witResponse) {
+    const defaultResponse = "I am not that smart yet. Have you tried Google it?";
+    if(!witResponse.entities.intent || witResponse.entities.intent.length === 0)
+    {
+        return defaultResponse;
+    }
     switch (witResponse.entities.intent[0].value) {
         case "weather_at_location":
             return await weatherApi.getWeatherInfo(witResponse.entities.location[0].value);
             break;
         default:
-            return "I am not that smart yet. Have you tried Google it?";
+            return defaultResponse;
     }
 }

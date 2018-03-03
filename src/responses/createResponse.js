4d24beea5ec141928cf84bb43fa796f666faf34d
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-03-03 18:55:45 +1300

    fix switch in create response

--- src/responses/createResponse.js
+++ src/responses/createResponse.js
@@ -2,10 +2,10 @@ import WeatherApi from "./WeatherApi";
 
 const weatherApi = new WeatherApi(process.env.OPEN_WEATHER_TOKEN);
 
-export function createResponse(witResponse) {
-    switch (witResponse.entities.intent.value) {
+export async function createResponse(witResponse) {
+    switch (witResponse.entities.intent[0].value) {
         case "weather_at_location":
-            return weatherApi.getWeatherInfo(witResponse.entities.location[0].value);
+            return await weatherApi.getWeatherInfo(witResponse.entities.location[0].value);
             break;
         default:
             return "I am not that smart yet. Have you tried Google it?";

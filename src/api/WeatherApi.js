c26bb921598f90c6cf8d1e9563402bc4b49a5108
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-03-03 17:17:59 +1300

    Parse weather info before creating message

--- src/api/WeatherApi.js
+++ src/api/WeatherApi.js
@@ -2,18 +2,22 @@ import Axios from "axios";
 
 const weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
 export default class WeatherApi {
-    constructor(apiToken){
+    constructor(apiToken) {
         this.apiToken = apiToken;
     }
 
     getWeatherInfo(location) {
         const getWeatherInfoUrl = `${weatherApiUrl}${location}&units=metric&APPID=${this.apiToken}`;
-        return Axios.get(getWeatherInfoUrl).then((response) => {
-            const weatherCondition = response.weather[0].description;
-            const temperature = response.main.temp;
-            return `Current temperature in ${response.name} is ${temperature} and it is ${weatherCondition}`;
-        }).catch((error) => {
-            return error.message;
-        })
+        return Axios.get(getWeatherInfoUrl)
+            .then((response) => {
+                return JSON.parse(response);
+            })
+            .then((data) => {
+                const weatherCondition = data.weather[0].description;
+                const temperature = data.main.temp;
+                return `Current temperature in ${data.name} is ${temperature} and it is ${weatherCondition}`;
+            }).catch((error) => {
+                return error.message;
+            })
     }
 }

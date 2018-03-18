5d0b79c3e33dfe65aebe3c8b14e2ebe3c8a55e4b
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-03-18 22:12:46 +1300

    Refactor api call with axios

--- src/services/WeatherApi.js
+++ src/services/WeatherApi.js
@@ -1,17 +1,20 @@
 import Axios from "axios";
 
-const weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+const weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather";
 export default class WeatherApi {
     constructor(apiToken) {
         this.apiToken = apiToken;
     }
 
     getCurrentWeatherInfo(location) {
-        const getWeatherInfoUrl = `${weatherApiUrl}${location}&units=metric&APPID=${this.apiToken}`;
-        return Axios.get(getWeatherInfoUrl)
-            .then(response => response.data)
-            .catch((error) => {
-                throw new Error(`Couldn\'t find weather info for ${location}`)
-            });
+        return Axios.get(weatherApiUrl, {
+            params: {
+                q: location,
+                units: "metric",
+                APPID: this.apiToken
+            }
+        }).then(response => response.data).catch((error) => {
+            throw new Error(`Couldn\'t find weather info for ${location}`)
+        });
     }
 }

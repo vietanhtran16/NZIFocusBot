61a66a6c63c2f1dbe0df2cf5fb76d408d3245b4d
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-03-20 21:17:10 +1300

    Fix access token

--- src/services/FbApi.js
+++ src/services/FbApi.js
@@ -1,7 +1,8 @@
 import fb from "fb";
 
 export function getUser(id) {
-    fb.setAccessToken(accessToken);
+    fb.setAccessToken(process.env.FB_PAGE_ACCESS_TOKEN);
     return fb.api(id, {fields: ['name']})
-        .then(info => info);
+        .then(info => info)
+        .catch(error => console.log(error.message));
 }

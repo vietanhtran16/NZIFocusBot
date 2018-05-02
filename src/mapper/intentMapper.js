e10ebf5f9a292745cf8d70798bb1bf3d20488120
vietanhtran1996 <tranvietanh1996@gmail.com>
2018-05-02 22:19:21 +1200

    Fix

--- src/mapper/intentMapper.js
+++ src/mapper/intentMapper.js
@@ -1,8 +1,5 @@
 import { Intent } from "../enums/Intent";
-import weatherResponse from "../responses/weatherResponse";
 import greetUserResponse from "../responses/greetUserResponse";
-
-const WeatherResponse = new weatherResponse();
 
 export async function mapIntentWithResponse(message) {
     return "Chào bạn. Bạn gửi câu hỏi qua cho mình nhé";
